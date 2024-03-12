04017872b883e9226169b0cb4abd248bc9f95324
Chahid Ayoub <91095407+aubchahid@users.noreply.github.com>
2024-03-12 14:11:16 +0000

    Finishing Details

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,7 +3,7 @@ const { VITE_API_URL, MODE, VITE_UPLOADS_URL } = import.meta.env;
 const MODE_ENV = MODE;
 
 const ASSETS_URL =
-	MODE == 'development' ? 'http://127.0.0.1:8000' : 'http://neweraconnect.org:81/apis/erp/api/';
+	MODE == 'development' ? 'http://127.0.0.1:8000' : 'http://neweraconnect.org:81/apis/erp/api/public/';
 
 const API_URL = VITE_API_URL;
 
